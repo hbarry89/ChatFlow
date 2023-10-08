8b4f62080ebaf4688f7dcd7c830ffc898f924213
@hudabarry-wwMZPT <hudabarry@yahoo.com>
2023-10-08 15:32:21 -0400

    connect backend and frontend, and openai API

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+
+fetch("http://127.0.0.1:5000/ask")
+  .then(response => response.json())
+  .then(data => console.log(data.answers));
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h1>Hello</h1>
   );
 }
 
--- server/server.py
+++ server/server.py
@@ -1,9 +1,28 @@
-from flask import Flask
+import openai
+openai.api_key = "you-wish"
+from flask import Flask, request, jsonify
+from flask_cors import cross_origin
 
 app = Flask(__name__)
 
-@app.route('/ask')
+@app.route("/ask")
+@cross_origin()
 def ask():
-    return {"answers:": ["Yes", "No", "Maybe"]}
+    completions = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt="good morning",
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        temperature=0.5
+    )
+    message = completions.choices[0].text
+    return {"answers": message}
 
-app.run()
+# @app.route('/ask')
+# @cross_origin()
+# def ask():
+#     return {"answers:": ["Yes", "No", "Maybe"]}
+
+if __name__ == "__main__":
+    app.run(debug=True)
